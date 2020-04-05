e1e34d61acd4a2c460474e8e7491dbe37600f170
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-05 11:47:30 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Days from './com'
+import Days from './components/Days'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Days />
      
       </header>
     </div>
---
+++ src/components/Days.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+
+export default class Days extends Component {
+    render() {
+
+        const url = `"api.openweathermap.org"=${process.env.REACT_APP_API_KEY}`;
+        return (
+            <div>
+                <h1>Check the Weather</h1>
+                
+            </div>
+        )
+    }
+}
