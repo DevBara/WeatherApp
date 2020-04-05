c300137fea294d124ca996ddc2753b6e1bbdef8a
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-05 11:50:33 -0500

    didMount

--- src/components/Days.js
+++ src/components/Days.js
@@ -3,9 +3,20 @@ import React, { Component } from 'react'
 
 
 export default class Days extends Component {
+    state={
+        dailyData:[]
+    }
+
+    componentDidMount = () => {
+        const url = `"api.openweathermap.org"=${process.env.REACT_APP_API_KEY}`;
+
+        fetch(url)
+            .then(res => res.json)())
+            .then (data => console.log("Data loaded",data.list))
+
+    }
     render() {
 
-        const url = `"api.openweathermap.org"=${process.env.REACT_APP_API_KEY}`;
         return (
             <div>
                 <h1>Check the Weather</h1>
