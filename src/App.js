ca15c5e8578170e392f9db028b7ec562d68c6936
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-10 12:26:40 -0500

    create pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import './App.css';
-import Days from './components/Days'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import CurrentDay from './components/CurrentDay'
+import DayTwo from './components/DayTwo'
+import DayThree from './components/DayThree'
+import DayFour from './components/DayFour'
+import DayFive from './components/DayFive'
 
 
 
@@ -8,9 +13,50 @@ export default class App extends Component {
 
   render() {
     return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to ="/"> Today's Forecast</Link>
+              </li>
+              <li>
+                <Link to ="/daytwo"> Day Two Forecast</Link>
+              </li>
+              <li>
+                <Link to ="/daythree"> Day Three Forecast</Link>
+              </li>
+              <li>
+                <Link to ="/dayfour"> Day Four Forecast</Link>
+              </li>
+              <li>
+                <Link to ="/dayfive"> Day Five Forecase</Link>
+              </li>
+            </ul>
+          </nav>
+          
+          <Switch>
+            <Route path ="/today">
+              <CurrentDay />
+            </Route>
+            <Route path ="/daytwo">
+              <DayTwo />
+            </Route>
+            <Route path ="/daythree">
+              <DayThree />
+            </Route>
+            <Route path ="/dayfour">
+              <DayFour />
+            </Route>
+            <Route path ="/dayfive">
+              <DayFive />
+            </Route>
+          </Switch>
+        </div>
       <div className='App'>
-        <Days />
+        
       </div>
+    </Router>
     )
   }
 }
